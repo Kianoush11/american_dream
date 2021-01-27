f4c8712b05eaffa9fe6e96c8298e131a94cbd9c7
kavisho <76555617+kavisho@users.noreply.github.com>
2021-01-27 14:50:40 +0330

    Update index.js

--- index.js
+++ index.js
@@ -73,45 +73,45 @@ bot.on("message",message=>{
   }
 });
 
+const talkedRecently = new Set();
 
 bot.on("message",message=>{
 
   const name = message.author.username;
-  const talkedRecently = new Set();
+  
 
   if (talkedRecently.has(message.author.id)) {
     message.channel.send("یوزر گرامی لطفا بعدا دوباره تلاش کنید - " + message.author);
 } else {
 
   if (message.content === 'g.hub') {
+    var role = message.guild.roles.find(role => role.id === "803653309755162634");
+    message.author.addRole(role);
+    message.react('☑️');
+    message.channel.send(name + " Aziz hub shoma sakhteh mishavad.")
     message.guild.createChannel(name + "-hub", {
       type: 'text',
+      overwritePermissions: [
+        {
+          id: message.guild.id,
+          allow: ['SEND_MESSAGES'],
+      },
+      {
+          id: message.author.id,
+          allow: ['MANAGE_CHANNELS'],
+      },
+      ]
     }).then(channel => {
       setTimeout(() => {
         channel.send("هاب شما ساخته شد \n حتما چنل خود را به میل خود تنظیم کنید \n مطالب غیر اخلاقی پست نکنید \n هاب خود را مثبت هجده و یا حالت اخبار نکنید \n و اگر فعالیت کنید تیک میگیرید و اگر نه چنل شما پاک میشه");
-        channel.overwritePermissions([
-          {
-            id: message.guild.id,
-            allow: ['SEND_MESSAGES'],
-        },
-        {
-            id: message.author.id,
-            allow: ['MANAGE_CHANNELS'],
-        },
-  
-        ]);
-      }, 7000);
+      }, 5000);
       setTimeout(() => {
         const catcat = '803631413127282739'
         channel.setParent(catcat);
     }, 5000);
     
     });
-    var role = message.guild.roles.find(role => role.id === "803653309755162634");
-    message.member.addRole(role);
-    message.react('☑️');
-    message.channel.send(name + " Aziz hub shoma sakhteh shod.")
-
+    
      // Adds the user to the set so that they can't talk for a minute
      
 
