ed5aa7c8ff4f563327da88fa60c14c133c20cf09
kavisho <76555617+kavisho@users.noreply.github.com>
2021-02-02 17:23:10 +0330

    zat

---
+++ cmd/drink.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js')
+
+
+const ab = [
+   
+    "شما یه لیوان آب انگور زدی", 
+    "آب کیر خوبی بود", 
+    "عرق نخور", 
+    "عجب ویسکی ای زدی ناموسا", 
+
+
+
+
+];
+
+const talkedRecently = new Set();
+
+exports.run = (bot, msg, params) => {
+
+    
+    if (talkedRecently.has(msg.author.id)) {
+        msg.channel.react('❌')
+} else {
+
+
+  //var invite = new Discord.RichEmbed()
+          //.setTitle("**𝐺𝑟𝑒𝑒𝑛𝐻𝑖𝑙𝑙𝑠**", true)
+          //.addField("__**" + "Developed In : " + "**__", ":flag_ir: by Kavisho with :heart:", true)
+          //.setFooter("𝐺𝑟𝑒𝑒𝑛𝐻𝑖𝑙𝑙𝑠")
+          //.setColor('RANDOM')
+          //<:frozen:780376909451624468>
+  msg.channel.send("<:harhar:806054502519799859><:bezan_shadshy:806125619070107648>");
+  msg.channel.send(ab[Math.floor(Math.random() * ab.length)]);
+
+};
+
+talkedRecently.add(msg.author.id);
+setTimeout(() => {
+   //Removes the user from the set after a minute
+  talkedRecently.delete(msg.author.id);
+}, 300000);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['info'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "drink",
+  description: "Bot Info",
+  usage: "drink"
+};
---
+++ cmd/smoke.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js')
+
+
+const ab = [
+   
+    "سیگار بهمن هم شد سیگار؟", 
+    "عجب توتونی داره", 
+    "سیگار توهمی میدی دستم خارکسه؟", 
+    "واو سیگار وینستون", 
+
+
+
+
+];
+
+const talkedRecently = new Set();
+
+exports.run = (bot, msg, params) => {
+
+    
+    if (talkedRecently.has(msg.author.id)) {
+        msg.channel.react('❌')
+} else {
+
+
+  //var invite = new Discord.RichEmbed()
+          //.setTitle("**𝐺𝑟𝑒𝑒𝑛𝐻𝑖𝑙𝑙𝑠**", true)
+          //.addField("__**" + "Developed In : " + "**__", ":flag_ir: by Kavisho with :heart:", true)
+          //.setFooter("𝐺𝑟𝑒𝑒𝑛𝐻𝑖𝑙𝑙𝑠")
+          //.setColor('RANDOM')
+          //<:frozen:780376909451624468>
+  msg.channel.send("<:kachal:806044487675281409><:sigar:806047101863591936>");
+  msg.channel.send(ab[Math.floor(Math.random() * ab.length)]);
+
+};
+
+talkedRecently.add(msg.author.id);
+setTimeout(() => {
+   //Removes the user from the set after a minute
+  talkedRecently.delete(msg.author.id);
+}, 300000);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['info'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "smoke",
+  description: "Bot Info",
+  usage: "smoke"
+};
